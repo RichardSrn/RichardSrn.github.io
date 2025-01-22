ce3cb3b8c803a304acfebc8dd1ea6dd36da79d02
RichardSrn <serrano.richard@protonmail.com>
2025-01-22 12:30:52 +0100

    update

---
+++ wp-content/themes/plover/core/assets/js/block-supports/shadow/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('plover-components', 'plover-icons', 'plover-utils', 'react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '3cd6b798b286648cd896');
