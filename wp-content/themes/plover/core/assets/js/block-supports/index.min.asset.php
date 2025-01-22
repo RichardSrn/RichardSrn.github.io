486d9ae9314fccfcb89868cdc9ce77d58bbae4fd
RichardSrn <serrano.richard@protonmail.com>
2025-01-22 12:30:52 +0100

    update

---
+++ wp-content/themes/plover/core/assets/js/block-supports/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('plover-data', 'plover-utils', 'react', 'wp-blocks', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => 'b030894653c1cfd5c68d');
---
+++ wp-content/themes/plover/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-plover/plover-theme',
+        'pretty_version' => 'v1.3.0',
+        'version' => '1.3.0.0',
+        'reference' => 'a8163346fac77be100e643712cc5e97eb278c1d2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wp-plover/plover-theme' => array(
+            'pretty_version' => 'v1.3.0',
+            'version' => '1.3.0.0',
+            'reference' => 'a8163346fac77be100e643712cc5e97eb278c1d2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
