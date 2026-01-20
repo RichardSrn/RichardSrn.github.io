d042572815893c52aac125c634b113884c21a19a
RichardSrn <serrano.richard@protonmail.com>
2026-01-20 11:46:06 +0100

    update

--- wp-content/themes/plover/core/README.md
+++
@@ -1,7 +0,0 @@
-# Plover Core
-
-Framework for Plover Kit & Plover Theme.
-
-- [GitHub Repo](https://github.com/wp-plover/core)
-- [License: GPLv2 or later](./LICENSE)
-- [Changelog](./CHANGELOG)
--- wp-content/themes/plover/core/assets/js/block-supports/index.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('plover-data', 'plover-utils', 'react', 'wp-blocks', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => 'b030894653c1cfd5c68d');
